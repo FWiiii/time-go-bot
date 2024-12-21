1fc186b58572b03e9307d58fcca6e0938cea1315
FWiiii <843189132@qq.com>
2024-12-21 16:01:29 +0800

    chore:test corn

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,8 @@
 import process from 'node:process'
 
-export const TOKEN = process.env.TOKEN!
+// export const TOKEN = process.env.TOKEN!
 export const BOT_TOKEN = process.env.BOT_TOKEN!
 
-if (!TOKEN || !BOT_TOKEN) {
+if (!BOT_TOKEN) {
   throw new Error('missing env')
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
+import { CronJob } from 'cron'
 import { Telegraf } from 'telegraf'
+
 import { BOT_TOKEN } from './env'
 
 export const bot = new Telegraf(BOT_TOKEN)
 
 bot.start(ctx => ctx.reply('Welcome!'))
 bot.help(ctx => ctx.reply('Help text...'))
+
+let count = 0
+
+const job = new CronJob(
+  // `${Math.floor(Math.random() * 120 + 60)} * * * *`, // Every 1-3 hours randomly
+  '*/30 * * * * *',
+  () => {
+    bot.telegram.sendMessage(1050365777, `Hello, world! ${count++}`)
+  }, // onTick
+  null, // onComplete
+  true, // start
+  'Asia/Shanghai', // timeZone
+)
+job.start()
